9fef6343a26ed97808507b58a5e3e141b56db85e
hz19114719 <chenguoyu@huize.com>
2019-12-17 18:30:25 +0800

    完成体系模块主模块逻辑

--- wandroid/src/main/java/com/cgy/wandroid/ui/main/tree/treeinfo/TreeInfoFragment.kt
+++ wandroid/src/main/java/com/cgy/wandroid/ui/main/tree/treeinfo/TreeInfoFragment.kt
@@ -14,6 +14,9 @@ import com.cgy.wandroid.mvp.contract.TreeInfoContract
 import com.cgy.wandroid.mvp.presenter.TreeInfoPresenter
 
 import com.cgy.wandroid.R
+import com.cgy.wandroid.ui.main.home.adapter.ArticleAdapter
+import com.cgy.wandroid.weight.DefineLoadMoreView
+import com.kingja.loadsir.core.LoadService
 
 
 /**
@@ -22,9 +25,18 @@ import com.cgy.wandroid.R
  * @date: 2019/12/17 16:32
  */
 class TreeInfoFragment : BaseFragment<TreeInfoPresenter>(), TreeInfoContract.View {
+    lateinit var loadsir: LoadService<Any>
+    lateinit var adapter: ArticleAdapter
+    private var initPageNo = 0 //注意,体系页码从0开始的
+    private var pageNo: Int = initPageNo //注意,体系页码从0开始的
+    private var cid: Int = 0
+    private var footView: DefineLoadMoreView? = null
     companion object {
         fun newInstance(): TreeInfoFragment {
+            val args = Bundle()
+            args.putInt("cid", cid)
             val fragment = TreeInfoFragment()
+            fragment.arguments = args
             return fragment
         }
     }
